0bc88d321ebb56da3d145c5cd50a4599de478dde
ahmad.sj <ahmad-alsarraj@hotmail.com>
2026-06-24 15:32:59 +0300

    added back box collision detection

--- src/classes/CollisionManager.js
+++ src/classes/CollisionManager.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { getClosestPoint3d } from "../helpers";
 
 export class CollisionManager {
   _items = {};
@@ -15,11 +16,54 @@ export class CollisionManager {
     this._items[Object.keys(item)[0]] = Object.values(item)[0];
   }
 
+  checkAABBCollision(ball, item) {
+    // 1. إيجاد أقرب نقطة على الصندوق لمركز الكرة
+    const closestPoint = getClosestPoint3d(item, ball);
+
+    // 2. حساب المسافة بين مركز الكرة وهذه النقطة
+    const distance = Math.sqrt(
+      (closestPoint.x - ball.position.x) ** 2 +
+        (closestPoint.y - ball.position.y) ** 2 +
+        (closestPoint.z - ball.position.z) ** 2,
+    );
+
+    // 3. إذا كانت المسافة أقل من نصف القطر، فهناك تصادم
+    return {
+      isColliding: distance < ball.radius,
+      closestPoint: closestPoint,
+      distance: distance,
+    };
+  }
+
+  checkBoxCollision(item, ball) {
+    const collision = this.checkAABBCollision(ball, item);
+
+    if (collision.isColliding) {
+      // حساب سهم الدفع (Normal)
+      const overlap = ball.radius - collision.distance;
+
+      // اتجاه الدفع من النقطة الأقرب باتجاه مركز الكرة
+      const direction = {
+        x: (ball.position.x - collision.closestPoint.x) / collision.distance,
+        y: (ball.position.y - collision.closestPoint.y) / collision.distance,
+        z: (ball.position.z - collision.closestPoint.z) / collision.distance,
+      };
+
+      // إعادة تموضع الكرة خارج الجسم تماماً
+      const ballNewX = ball.position.x + direction.x * overlap;
+      const ballNewY = ball.position.y + direction.y * overlap;
+      const ballNewZ = ball.position.z + direction.z * overlap;
+      const ballNewPosition = new THREE.Vector3(ballNewX, ballNewY, ballNewZ);
+      ball.position = ballNewPosition;
+
+      // عكس السرعة لمحاكاة الارتداد
+      ball._linearVelocity.negate().multiplyScalar(0.5);
+    }
+  }
+
   checkSlopeCollision() {
     const ball = this._items["ball"];
     const slope = this._items["slope"];
-
-    // console.log("angle", THREE.MathUtils.radToDeg(Math.acos(slope.normal.y)));
 
     // Get theoretical ground height at ball's center (X,Z)
     const slopeY = slope.getHeightAt(ball.position.x, ball.position.z);
@@ -70,6 +114,13 @@ export class CollisionManager {
     const ball = this._items["ball"];
     const ground = this._items["ground"];
     const forceManager = this._forceManager;
+
+    // Check collision with boxes
+    for (const item of Object.values(this._items)) {
+      if (item.constructor.name === "Box") {
+        this.checkBoxCollision(item, ball);
+      }
+    }
 
     const onSlope = this.checkSlopeCollision();
 
